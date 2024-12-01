4c19cfd1a5b4ff9cbebe7a9d5a1adef6b5dc8cf7
U <80122730+und1n3@users.noreply.github.com>
2024-12-01 20:48:14 +0100

    h

--- day1.py
+++ day1.py
@@ -1,1 +1,18 @@
-#
+#open file
+f = open('input_1.txt','r')
+lines = f.readlines()
+list_1 = []
+list_2 = []
+for line in lines:
+    words = line.strip().split(' ')
+    list_1.append(words[0])
+    list_2.append(words[-1])
+
+distances = []
+n=len(list_1)
+while n>0:
+    distances.append(abs(min(list_1)-min(list_2)))
+    del list_1[list_1.index(min(list_1))]
+    del list_2[list_2.index(min(list_2))]
+    n-=1
+print(sum(distances))
