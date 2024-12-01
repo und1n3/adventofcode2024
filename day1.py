9e5aed978912525c1f0227bfa9678d33613e1c21
U <80122730+und1n3@users.noreply.github.com>
2024-12-01 20:07:52 +0000

    day one

--- day1.py
+++ day1.py
@@ -1,13 +1,17 @@
-#open file
-f = open('input_1.txt','r')
-lines = f.readlines()
-list_1 = []
-list_2 = []
-for line in lines:
-    words = line.strip().split(' ')
-    list_1.append(words[0])
-    list_2.append(words[-1])
+from collections import Counter
 
+def get_lists():  
+    #open file
+    f = open('input_1.txt','r')
+    lines = f.readlines()
+    list_1 = []
+    list_2 = []
+    for line in lines:
+        words = line.strip().split(' ')
+        list_1.append(int(words[0]))
+        list_2.append(int(words[-1]))
+    return list_1,list_2
+list_1,list_2 = get_lists()
 distances = []
 n=len(list_1)
 while n>0:
@@ -15,4 +19,13 @@ while n>0:
     del list_1[list_1.index(min(list_1))]
     del list_2[list_2.index(min(list_2))]
     n-=1
-print(sum(distances))
+print('answer 1: ',sum(distances))
+# puzzle 2
+list_1,list_2 = get_lists()
+elements_1 = set(list_1) 
+counter_2 = Counter(list_2)
+similarity = 0
+
+for el in elements_1:
+    similarity += el*counter_2[el]
+print('answer 2: ',similarity)
